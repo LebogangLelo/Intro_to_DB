62bd4d126993718b0dccf747b3e6e20dacc2c06f
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-10-20 00:03:22 +0200

    Print the full description of a books table

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,17 @@
 USE alx_book_store;
 
--- prints the full description of the table books
-SHOW COLUMNS FROM books;
+
+-- Query to print the full description of the books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    information_schema.columns 
+WHERE 
+    table_schema = DATABASE() 
+    AND table_name = 'books';
+
